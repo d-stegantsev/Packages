b5d3ffe18ba9e2c76544b45a466e8e7f5741e02e
d-stegantsev <stegan296@gmail.com>
2024-10-05 23:19:08 +0300

    Create LOG_UTIL.sql

---
+++ LOG_UTIL.sql
@@ -0,0 +1,13 @@
+create or replace PACKAGE log_util AS
+
+    PROCEDURE log_start (p_proc_name IN VARCHAR2,
+                         p_text      IN VARCHAR2 DEFAULT NULL);
+    
+    PROCEDURE log_finish (p_proc_name IN VARCHAR2,
+                          p_text      IN VARCHAR2 DEFAULT NULL);
+    
+    PROCEDURE log_error  (p_proc_name IN VARCHAR2,
+                          p_sqlerrm   IN VARCHAR2,
+                          p_text      IN VARCHAR2 DEFAULT NULL);
+
+END log_util;
